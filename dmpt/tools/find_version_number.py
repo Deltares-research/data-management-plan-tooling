8aa11efb81199ff1b1c00950d61eeb81ae9f9142
Aris Nivorlis <anivorlis@gmail.com>
2024-10-17 14:59:04 +0200

    raise value error instead of none
    Incorrect filenames will raise value error. Version number is now an int.

--- dmpt/tools/find_version_number.py
+++ dmpt/tools/find_version_number.py
@@ -1,9 +1,18 @@
 import re
 
 
-def find_version_number(filename: str) -> tuple[str, str]:
+def find_version_number(filename: str) -> tuple[int, int]:
+    """Find the version number of the filename. 
+    The version number is expected to be in the format v{major}.{minor}.
+
+    Args:
+        filename (str): The name of the file
+
+    Returns:
+        tuple[int, int]: Tuple of major and minor version numbers
+    """    
     version_pattern = r'v(?P<major>\d+)\.(?P<minor>\d+)'
     match_obj = re.search(version_pattern, filename)
     if match_obj is None:
-        return None, None
-    return match_obj['major'], match_obj['minor']
+        raise ValueError(f"No version number found in filename: {filename}")
+    return int(match_obj['major']), int(match_obj['minor'])
--- tests/test_match.py
+++ tests/test_match.py
@@ -5,18 +5,26 @@ from dmpt.tools.find_version_number import find_version_number
 
 # Use pytest.mark.parametrize to test multiple inputs.
 @pytest.mark.parametrize("filename, expected_major_version, expected_minor_version", [
-    ("123456789-BGS_v1.1-data-management-plan.docx", '1', '1'),
-    ("123456789-GEO_v1.2-data-management-plan.docx", '1', '2'),
-    ("123456789-HYE_v1.3-data-management-plan.docx", '1', '3'),
-    ("123456789-ZKS_v2.4-data-management-plan.docx", '2', '4'),
-    ("123456789-ZWS_v2.9-data-management-plan.docx", '2', '9'),
-    ("123456789-DSC_v5.10-data-management-plan.docx", '5', '10'),
-    ("123456789-BGS_v10.100-data-management-plan.docx", '10', '100'),
-    ("randomfile.docx", None, None),  # Case where no match is expected.
-    ("", None, None),  # empty string
-    ("vasds", None, None)  # v but no version number
+    ("123456789-BGS_v1.1-data-management-plan.docx", 1, 1),
+    ("123456789-GEO_v1.2-data-management-plan.docx", 1, 2),
+    ("123456789-HYE_v1.3-data-management-plan.docx", 1, 3),
+    ("123456789-ZKS_v2.4-data-management-plan.docx", 2, 4),
+    ("123456789-ZWS_v2.9-data-management-plan.docx", 2, 9),
+    ("123456789-DSC_v5.10-data-management-plan.docx", 5, 10),
+    ("123456789-BGS_v10.100-data-management-plan.docx", 10, 100)
 ])
 def test_find_version_number(filename: str, expected_major_version: str, expected_minor_version: str) -> None:
     major_version, minor_version = find_version_number(filename)
     assert major_version == expected_major_version
     assert minor_version == expected_minor_version
+
+
+@pytest.mark.parametrize("filename", [
+    ("randomfile.docx"),  # Case where no match is expected.
+    (""),  # Empty string
+    ("vasds"),  # 'v' but no version number
+    ("v1asds")  # 'v' but no version number
+])
+def test_find_version_number_exception(filename: str) -> None:
+    with pytest.raises(ValueError):
+        find_version_number(filename)
